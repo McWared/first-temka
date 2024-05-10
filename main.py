8973ac96aba182961c8180f1b2239d99d30ede77
McWared <mrbaymiks@gmail.com>
2024-05-10 19:07:41 +0300

    __init__

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import tkinter as tk
 
 class Calculator:
     def __init__(self):
-        pass
+        self.window = tk.Tk()
+        self.window.geometry = ('375x667')
+        self.window.title('Calculator')
+
+    def run(self):
+        self.window.mainloop()
