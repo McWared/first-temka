aa1e755e7b3bb7d02c0e745e964754267bb38f81
dimakuzko <kuzko.dima.2018@gmail.com>
2024-05-12 12:21:47 +0300

    if

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-#Main file for Calculator
+# Main file for Calculator
 import tkinter as tk
 
-class Calculator:
+
+class Calculator_UA:
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry = ('375x667')
-        self.window.title('Calculator')
+        self.window.title('Calculator UA')
 
     def run(self):
         self.window.mainloop()
+
+
+if __name__ == "__main__":
+    calculator_ua = Calculator_UA()
+    calculator_ua.run()
