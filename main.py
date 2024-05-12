f7a80c31acb409439981476336855bc851ef3b46
McWared <mrbaymiks@gmail.com>
2024-05-12 20:07:06 +0300

    -_create_digits_buttons

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ class Calculator_UA:
         self.display_label = self._create_display_label()
         self.buttons_frame = self._create_buttons_frame()
 
+        self.digits = {
+            7: (1,1), 8: (1,2), 9: (1,3),
+            4: (2,1), 5: (2,2), 6: (2,3),
+            1: (3,1), 2: (3,2), 3: (3,3),
+            0: (4,2), '.': (4,3)
+        }
+        self._create_digits_buttons()
+
     def _create_display_label(self):
         label = Label(self.calculation_frame, text=self.expression, anchor=E, fg='black', bg='lightgray', font='Arial')
         label.pack(expand=True, fill='both')
@@ -31,8 +39,10 @@ class Calculator_UA:
         return frame
 
 
-    def _create_buttons(self):
-        pass
+    def _create_digits_buttons(self):
+        for digit, grid_value in self.digits.items():
+            button = Button(self.buttons_frame, text=str(digit))
+            button.grid(column=grid_value[1], row=grid_value[0])
 
     def run(self):
         self.window.mainloop()
