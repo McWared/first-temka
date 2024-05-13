1053f5be6b20730743868907da5062fee66ef89b
McWared <mrbaymiks@gmail.com>
2024-05-13 14:22:56 +0300

    add doc strings

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-# Main file for Calculator
+"""Main file for Calculator"""
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 
 class Calculator_UA:
-    def __init__(self):
+    """Calculator class that defining all stuff"""
+    def __init__(self) -> None:
+        """
+        Init object
+        """
         self.window = Tk()
         self.window.geometry('400x520')
         self.window.title('Calculator UA')
@@ -30,7 +34,10 @@ class Calculator_UA:
 
         self.create_buttons()
 
-    def create_buttons(self):
+    def create_buttons(self) -> None:
+        """
+        Creates all buttons
+        """
         self.create_operations_buttons()
         self.create_equal_button()
         self.create_digits_buttons()
@@ -38,27 +45,42 @@ class Calculator_UA:
         self.create_all_clear_button()
         self.create_clear_button()
 
-    def create_display_label(self):
+    def create_display_label(self) -> Label:
+        """
+        Creates display label
+        """
         label = Label(self.calculation_frame, text=self.expression, anchor=E, fg='black', bg='lightgray', font=('Arial', 24, 'bold'))
         label.pack(expand=True, fill='both')
         return label
     
-    def create_calculation_frame(self):
+    def create_calculation_frame(self) -> Frame:
+        """
+        Creates frame for the expression area
+        """
         frame = Frame(self.window, height='200')
         frame.pack(expand=True, fill='both')
         return frame
 
-    def create_buttons_frame(self):
+    def create_buttons_frame(self) -> Frame:
+        """
+        Creates frame for the buttons area
+        """
         frame = Frame(self.window, height='320')
         frame.pack(expand=True, fill='both')
         return frame
 
-    def create_digits_buttons(self):
+    def create_digits_buttons(self) -> None:
+        """
+        Creates buttons for all digits
+        """
         for digit, grid_value in self.digits.items():
             button = Button(self.buttons_frame, text=str(digit), borderwidth=0, font=('Arial', 24, 'bold'), command=lambda x=digit: self.update_expression(x))
             button.grid(row=grid_value[0], column=grid_value[1], sticky=NSEW)
 
-    def create_operations_buttons(self):
+    def create_operations_buttons(self) -> None:
+        """
+        Creates buttons for all operators
+        """
         i = 0
 
         for operator, symbol in self.operations.items():
@@ -66,52 +88,88 @@ class Calculator_UA:
             button.grid(row=i, column=4, sticky=NSEW)
             i += 1
 
-    def create_equal_button(self):
+    def create_equal_button(self) -> None:
+        """
+        Creates button for equal
+        """
         button = Button(self.buttons_frame, text='=', borderwidth=0, font=('Arial', 24, 'bold'), command=lambda: self.evaluation())
         button.grid(row=4, column=4, sticky=NSEW)
 
-    def create_parentheses_buttons(self):
+    def create_parentheses_buttons(self) -> None:
+        """
+        Creates buttons for parentheses
+        """
         button_1 = Button(self.buttons_frame, text='(', borderwidth=0, font=('Arial', 24, 'bold'), command=lambda: self.update_expression('('))
         button_1.grid(row=0, column=2, sticky=NSEW)
         button_2 = Button(self.buttons_frame, text=')', borderwidth=0, font=('Arial', 24, 'bold'), command=lambda: self.update_expression(')'))
         button_2.grid(row=0, column=3, sticky=NSEW)
 
-    def create_all_clear_button(self):
+    def create_all_clear_button(self) -> None:
+        """
+        Creates button for all clear
+        """
         button = Button(self.buttons_frame, text='C', borderwidth=0, font=('Arial', 24, 'bold'), command=lambda: self.all_clear())
         button.grid(row=0, column=1, sticky=NSEW)
 
-    def create_clear_button(self):
+    def create_clear_button(self) -> None:
+        """
+        Creates button for clear the last symbol
+        """
         button = Button(self.buttons_frame, text='<<', borderwidth=0, font=('Arial', 24, 'bold'), command=lambda: self.clear())
         button.grid(row=4, column=1, sticky=NSEW)
         
-    def update_label(self):
+    def update_label(self) -> None:
+        """
+        Updates the text on the label
+        """
         self.display_label.config(text=self.expression)
 
-    def update_expression(self, value):
+    def update_expression(self, value) -> None:
+        """
+        Updates the expression, adding new symbols to it
+        - args: 
+            value: str | int
+        """
         self.expression += str(value)
         self.total_expression += str(value)
         self.update_label()
-
-    def append_operator(self, operator):
+        
+    def append_operator(self, operator) -> None:
+        """
+        Updates the expression, adding new operator symbol to it
+        - args: 
+            operator: str
+        """
         self.total_expression += operator
         self.expression = ''
         self.update_label()
 
-    def all_clear(self):
+    def all_clear(self) -> None:
+        """
+        Full clears the expression
+        """
         self.expression = ''
         self.update_label()
 
-    def clear(self):
+    def clear(self) -> None:
+        """
+        Deletes the last symbol in the expression
+        """
         self.expression = self.expression[:-1]
         self.update_label()
 
-    def evaluation(self):
+    def evaluation(self) -> None:
+        """
+        Evaluates the expression
+        """
         self.expression = str(eval(self.total_expression))
         self.update_label()
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Runs the window
+        """
         self.window.mainloop()
-
 
 if __name__ == "__main__":
     calculator_ua = Calculator_UA()
